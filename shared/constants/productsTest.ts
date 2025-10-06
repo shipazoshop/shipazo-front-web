f6ebb3a8a54a874b8319238496ad4cfc32bc1d21
byxmariux <mario.hm205@gmail.com>
2025-10-05 21:49:42 -0600

    feat: :sparkles: Se agrego tipado para las cards en productos
    Tipado de productos, modificado Products4.tsx se agrego la constante productsTest.ts y se modifico CurrencySelect para el funcionamiento.

---
+++ shared/constants/productsTest.ts
@@ -0,0 +1,49 @@
+import {Product} from "@/domain";
+
+
+export const arrayProducts:Product[]=[
+
+
+    {
+        "success": true,
+    "url": "https://www.amazon.com/-/es/Hanes-EcoSmart-Sudadera-sudadera-unidades/dp/B072QRN7HR/?_encoding=UTF8&pd_rd_w=jyOnY&content-id=amzn1.sym.9929d3ab-edb7-4ef5-a232-26d90f828fa5&pf_rd_p=9929d3ab-edb7-4ef5-a232-26d90f828fa5&pf_rd_r=8AY635S6QMVS18A1WQ7P&pd_rd_wg=HBtzW&pd_rd_r=131972f6-e2b5-4482-85d8-3e9f45633d09&ref_=pd_hp_d_btf_crs_zg_bs_7141123011",
+    "product_id": "B072QRN7HR",
+    "productData": {
+        "title": "Hanes Men's EcoSmart Fleece, Pullover Crewneck Sweatshirt, 1 or 2 Pack",
+        "description": "FLEECE TO FEEL GOOD ABOUT - This men's pullover sweatshirt is made with cotton sourced from American farms.\nSTAYS SOFT - Thick, sturdy fleece keeps this crewneck sweatshirt warm and cozy.\nUNISEX SIZING - This sweatshirt is designed with a standard men’s fit that also creates an effortless, oversized look for women. Female model is 5'8\" and wearing a size medium.\nMADE TO LAST - Double-needle stitching at the neck and armholes adds extra quality and sturdiness to this men's sweatshirt.\nCLASSIC SILHOUETTE - Basic crewneck sweatshirt shaping for that versatile look you love.\nBIG & TALL OPTIONS - This pullover sweatshirt is available in Big Men's sizing up to 3XL, so you can find your perfect fit.",
+        "price": "147.97",
+        "currency": "GTQ",
+        "brand": "Hanes",
+        "stock": true,
+        "weight": "0.00",
+        "dimensions": "N/A",
+        "images": [
+            "https://m.media-amazon.com/images/I/81D+bCgn8hL._AC_SL1500_.jpg",
+            "https://m.media-amazon.com/images/I/81gtlYtDElL._AC_SL1500_.jpg",
+            "https://m.media-amazon.com/images/I/61MAvTG-FsL._AC_SL1500_.jpg",
+            "https://m.media-amazon.com/images/I/71Bfqfr+29L._AC_SL1500_.jpg",
+            "https://m.media-amazon.com/images/I/71TNJRvJ4hL._AC_SL1500_.jpg",
+            "https://m.media-amazon.com/images/I/51BTcTs0ulL._AC_SL1500_.jpg",
+            "https://m.media-amazon.com/images/I/51BTcTs0ulL._AC_SL1500_.jpg"
+        ],
+        "price_details": {
+            "base_price_usd": "8.84",
+            "exchange_rate": "7.66",
+            "base_price_gtq": "67.71",
+            "taxes": {
+                "duty_rate": "12.5%"
+            },
+            "operational_costs": {
+                "flete": "38.30",
+                "dai": "8.46",
+                "iva": "13.74",
+                "servicio_calshop": "12.82",
+                "comision_parcela": "6.95",
+                "valor_cif": "106.00"
+            },
+            "final_price": "147.97"
+        }
+    }
+}
+
+    ]
