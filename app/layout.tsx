0a5064caa1fca2769c787a85c02b98e6638985d3
Cristian Suar <cristian.suar@multigroup.com>
2025-11-03 22:48:34 -0600

    Optimize font loading with next/font
    Replaced Google Fonts CDN imports with next/font for self-hosted, automatically optimized fonts (Inter and Poppins). Updated SCSS variables to use CSS custom properties from next/font, improving performance and reducing external requests.

--- app/layout.tsx
+++ app/layout.tsx
@@ -4,23 +4,33 @@ import Modals from "./Modals";
 import "../public/scss/main.scss";
 import "photoswipe/dist/photoswipe.css";
 import type { ReactNode } from "react";
+import { Inter, Poppins } from "next/font/google";
 
 type RootLayoutProps = {
   children: ReactNode;
 };
 
+// Optimize font loading with next/font for better performance
+// Fonts are self-hosted and automatically optimized
+const inter = Inter({
+  subsets: ["latin"],
+  weight: ["400", "500", "600", "700"],
+  variable: "--font-inter",
+  display: "swap",
+  preload: true,
+});
+
+const poppins = Poppins({
+  subsets: ["latin"],
+  weight: ["300", "400", "500", "600", "700"],
+  variable: "--font-poppins",
+  display: "swap",
+  preload: true,
+});
+
 const RootLayout = ({ children }: RootLayoutProps) => {
   return (
-    <html lang="en">
-      <head>
-        <link rel="preconnect" href="https://fonts.googleapis.com" />
-        <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="" />
-        <link
-          href="https://fonts.googleapis.com/css2?family=Inter:wght@400;500;600;700&family=Poppins:wght@300;400;500;600;700&display=swap"
-          rel="stylesheet"
-        />
-        <link href="https://fonts.cdnfonts.com/css/helvetica-neue-55" rel="stylesheet" />
-      </head>
+    <html lang="en" className={`${inter.variable} ${poppins.variable}`}>
       <body>
         <ClientLayout />
         <div id="wrapper">
