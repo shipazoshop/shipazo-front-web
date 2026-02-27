bef5118d0d2ac67d45d9291571bd60dc9d84c275
Issei suar <cristsuar@gmail.com>
2026-02-26 18:28:17 -0600

    build fixed

--- presentation/components/analytics/Analytics.tsx
+++ presentation/components/analytics/Analytics.tsx
@@ -255,7 +255,7 @@ export default function Analytics() {
                       cx="50%"
                       cy="50%"
                       labelLine={false}
-                      label={(entry) => `${entry.name}: ${entry.value?.toFixed(1)}%`}
+                      label={(entry) => `${entry.name}: ${((entry?.value ?? 0) as number)?.toFixed(1)}%`}
                       outerRadius={80}
                       dataKey="value"
                       animationDuration={800}
